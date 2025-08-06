b2d751c55635b340e27975719a2922d2bc259e80
xSikio <xsikio1@gmail.com>
2025-08-05 19:56:55 -0500

    Calculadora terminada

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -4,11 +4,12 @@ import "../hojas-de-estilo/Boton.css";
 function Boton(props) {
 
     const esOperador = valor=> {
-        return isNaN(valor) && (valor != ".") && (valor != "=");
+        return isNaN(valor) && (valor !== ".") && (valor !== "=");
     };
     return ( 
-    <div className={`boton-contenedor ${esOperador(props.children) ? "operadores" :""}`.trimEnd()}>
-        {props.children}
+    <div className={`boton-contenedor ${esOperador(props.children) ? "operadores" :""}`.trimEnd()}
+    onClick={() => props.manejarClic(props.children)}> 
+    {props.children}
     </div>
 
     );
--- src/componentes/BotonClear.js
+++ src/componentes/BotonClear.js
@@ -2,7 +2,8 @@ import React from "react";
 import "../hojas-de-estilo/BotonClear.css"; 
 
 const BotonClear =(props) =>(
-    <div className="boton-clear"> {props.children}
+    <div className="boton-clear" onClick = {props.manejarClic}> 
+    {props.children}
      </div>
 )
 
